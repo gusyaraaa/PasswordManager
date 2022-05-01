7449b0aa418cf73ab97bc93af83fb3575e810d77
gusyaraaa <gusyaraaa@yandex.ru>
2022-05-01 19:16:09 +0300

    Added PasswordGenerator page

---
+++ client/src/components/screens/PasswordGenerator/PasswordGenerator.jsx
@@ -0,0 +1,11 @@
+import "./PasswordGenerator.scss";
+
+const PasswordGenerator = () => {
+	return (
+		<>
+			<h2 className="text">PasswordGenerator Page</h2>
+		</>
+	);
+};
+
+export default PasswordGenerator;
